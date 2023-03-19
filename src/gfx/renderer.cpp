b22a38082fb5df8459cf5a51e3c6ee00643d1704
Tearnote <marach5@gmail.com>
2023-03-19 21:21:09 +0000

    Add a denoiser

--- src/gfx/renderer.cpp
+++ src/gfx/renderer.cpp
@@ -23,37 +23,54 @@ void Renderer::draw(gfx::Camera const& _camera) {
 	auto frameAllocator = vuk::Allocator(frameResource);
 
 	// Compile shaders
-	static auto shaderCompiled = false;
-	if(!shaderCompiled) {
-		auto pci = vuk::PipelineBaseCreateInfo();
-		constexpr auto source = std::to_array<uint>({
+	static auto shadersCompiled = false;
+	if(!shadersCompiled) {
+		auto shaderPci = vuk::PipelineBaseCreateInfo();
+		constexpr auto shaderSource = std::to_array<uint>({
 #include "spv/shader.comp.spv"
-		});
-		pci.add_static_spirv(source.data(), source.size(), "shader.comp");
-		sys::s_vulkan->context.create_named_pipeline("shader_comp", pci);
+		                                            });
+		shaderPci.add_static_spirv(shaderSource.data(), shaderSource.size(), "shader.comp");
+		sys::s_vulkan->context.create_named_pipeline("shader_comp", shaderPci);
+
+		auto denoisePci = vuk::PipelineBaseCreateInfo();
+		constexpr auto denoiseSource = std::to_array<uint>({
+#include "spv/denoise.comp.spv"
+		                                            });
+		denoisePci.add_static_spirv(denoiseSource.data(), denoiseSource.size(), "denoise.comp");
+		sys::s_vulkan->context.create_named_pipeline("denoise", denoisePci);
 	}
 
 	// Create a rendergraph
 	auto rg = std::make_shared<vuk::RenderGraph>("simple");
-	rg->attach_image("image", vuk::ImageAttachment{
+	rg->attach_image("rt/blank", vuk::ImageAttachment{
 		.extent = vuk::Dimension3D::absolute(
 			sys::s_vulkan->swapchain->extent.width,
 			sys::s_vulkan->swapchain->extent.height
 		),
-		.format = vuk::Format::eR8G8B8A8Unorm,
+		.format = vuk::Format::eR16G16B16A16Sfloat,
+		.sample_count = vuk::Samples::e1,
+		.level_count = 1,
+		.layer_count = 1,
+	});
+	rg->attach_image("denoised/blank", vuk::ImageAttachment{
+		.extent = vuk::Dimension3D::absolute(
+			sys::s_vulkan->swapchain->extent.width,
+			sys::s_vulkan->swapchain->extent.height
+		),
+		.format = vuk::Format::eR16G16B16A16Sfloat,
 		.sample_count = vuk::Samples::e1,
 		.level_count = 1,
 		.layer_count = 1,
 	});
 	rg->attach_swapchain("swapchain", sys::s_vulkan->swapchain);
 	rg->add_pass(vuk::Pass{
-		.name = "screen clear",
+		.name = "raytracing",
 		.resources = {
-			"image"_image >> vuk::eComputeWrite >> "image_out",
+			"rt/blank"_image >> vuk::eComputeWrite >> "rt/out",
 		},
 		.execute = [&_camera](vuk::CommandBuffer& cmd) {
 			cmd.bind_compute_pipeline("shader_comp")
-			   .bind_image(0, 0, "image");
+				.bind_image(0, 0, "rt/blank");
 
 			struct Constants {
 				mat4 View;
@@ -71,15 +88,37 @@ void Renderer::draw(gfx::Camera const& _camera) {
 		},
 	});
 	rg->add_pass(vuk::Pass{
-		.name = "swapchain blit",
+		.name = "denoising",
 		.resources = {
-			"image_out"_image >> vuk::eTransferRead,
-			"swapchain"_image >> vuk::eTransferWrite >> "swapchain_out",
+			"rt/out"_image >> vuk::eComputeSampled,
+			"denoised/blank"_image >> vuk::eComputeWrite >> "denoised/out",
 		},
 		.execute = [](vuk::CommandBuffer& cmd) {
-			auto srcSize = cmd.get_resource_image_attachment("image_out").value().extent.extent;
+			cmd.bind_compute_pipeline("denoise")
+				.bind_image(0, 0, "rt/out").bind_sampler(0, 0, vuk::SamplerCreateInfo{
+					.magFilter = vuk::Filter::eLinear,
+					.minFilter = vuk::Filter::eLinear,
+					.addressModeU = vuk::SamplerAddressMode::eClampToEdge,
+					.addressModeV = vuk::SamplerAddressMode::eClampToEdge,
+				})
+				.bind_image(0, 1, "denoised/blank");
+
+			cmd.dispatch_invocations(
+				sys::s_vulkan->swapchain->extent.width,
+				sys::s_vulkan->swapchain->extent.height
+			);
+		},
+	});
+	rg->add_pass(vuk::Pass{
+		.name = "swapchain blit",
+		.resources = {
+			"denoised/out"_image >> vuk::eTransferRead,
+			"swapchain"_image >> vuk::eTransferWrite >> "swapchain/out",
+		},
+		.execute = [](vuk::CommandBuffer& cmd) {
+			auto srcSize = cmd.get_resource_image_attachment("denoised/out").value().extent.extent;
 			auto dstSize = cmd.get_resource_image_attachment("swapchain").value().extent.extent;
-			cmd.blit_image("image_out", "swapchain", vuk::ImageBlit{
+			cmd.blit_image("denoised/out", "swapchain", vuk::ImageBlit{
 				.srcSubresource = vuk::ImageSubresourceLayers{ .aspectMask = vuk::ImageAspectFlagBits::eColor },
 				.srcOffsets = {vuk::Offset3D{0, 0, 0}, vuk::Offset3D{int(srcSize.width), int(srcSize.height), 1}},
 				.dstSubresource = vuk::ImageSubresourceLayers{ .aspectMask = vuk::ImageAspectFlagBits::eColor },
