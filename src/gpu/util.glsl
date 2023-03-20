1ce870d0d87635c17c32c6d74d4d13448bee26a3
Tearnote <marach5@gmail.com>
2023-03-20 08:43:39 +0000

    Improve sample blending with invertible tonemapper

--- src/gpu/util.glsl
+++ src/gpu/util.glsl
@@ -3,4 +3,9 @@
 
 const float Pi = 3.14159265359;
 
+// https://gpuopen.com/learn/optimized-reversible-tonemapper-for-resolve/
+float Max3(float x, float y, float z) { return max(x, max(y, z)); }
+vec3 tonemap(vec3 c) { return c * (1.0 / (Max3(c.r, c.g, c.b) + 1.0)); }
+vec3 tonemapInvert(vec3 c) { return c * (1.0 / (1.0 - Max3(c.r, c.g, c.b))); }
+
 #endif //UTIL_GLSL
